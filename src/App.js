5a5f966e23abd86e2cd8086ed6a242701aa1fc7c
Victor Andrade <32168144@mackenzista.com.br>
2022-11-12 01:32:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>Olá React!</h1>
       <h2>Meu primeiro app com react</h2>
       <p>Olha só, estou criando meu primeiro app com react</p>
