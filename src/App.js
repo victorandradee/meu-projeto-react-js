15a412121c8f95fde27ae704772011e5e5fb5652
Victor Andrade <32168144@mackenzista.com.br>
2022-11-12 13:13:57 -0300

    Aprendendo Componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
 
 function App() {
+
+  const name = 'Victor' 
+  const newName = name.toUpperCase()
+
+  function sum(a,b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <h2>Meu primeiro app com react</h2>
       <p>Olha só, estou criando meu primeiro app com react</p>
      
+      <p>Soma: {sum(1, 2)}</p> 
+      <img src={url} alt='Minha imagem'/>
+      <p>Olha só, estou criando meu primeiro app com react, sou o {newName}</p>
+      <HelloWorld/>
+      <Frase/>
     </div>
   );
 }
