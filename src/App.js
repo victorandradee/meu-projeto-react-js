d2d24a4c016d4a910b5b1a44812d72a5ad0192e2
Victor Andrade <32168144@mackenzista.com.br>
2022-11-12 01:31:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Olá React!</h1>
+      <h2>Meu primeiro app com react</h2>
+      <p>Olha só, estou criando meu primeiro app com react</p>
+     
     </div>
   );
 }
